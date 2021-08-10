f7000d77bef38788265c16f878617267d7b08150
Ralitsa <ralitsanikolova@abv.bg>
2021-08-10 15:03:05 +0300

    Submission of 4/9 tasks on the topic of Interfaces and Abstraction.

---
+++ Interfaces and Abstraction/Telephony/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Telephony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] call = Console.ReadLine().Split(' ');
+
+            string[] browse = Console.ReadLine().Split(' ');
+
+            SmartPhone smartPhone = new SmartPhone(call, browse);
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+            foreach (var phoneNumber in call)
+            {
+                if (phoneNumber.Length == 10)
+                {
+                    Console.WriteLine(smartPhone.Calling(phoneNumber));
+                }
+                if (phoneNumber.Length == 7)
+                {
+                    Console.WriteLine(stationaryPhone.Calling(phoneNumber));
+                }
+            }
+
+            foreach (var address in browse)
+            {
+                Console.WriteLine(smartPhone.Browsing(address));
+            }
+        }
+    }
+}
+
+
---
+++ Interfaces and Abstraction/Telephony/SmartPhone.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Telephony
+{
+    class SmartPhone : ISmartphone
+    {
+        private string[] call;
+        private string[] browse;
+
+        public SmartPhone(string[] call, string[] browse)
+        {
+            this.call = call;
+            this.browse = browse;
+        }
+
+        public string Calling(string call)
+        {
+            if (Regex.IsMatch(call, @"^\d+$"))
+            {
+                return $"Calling... {call}";    
+            }
+           
+            else
+            {
+                return "Invalid number";
+            }
+        }
+        public string Browsing(string address)
+        {
+            if (Regex.IsMatch(address, @"^\D+$"))
+            {
+                return $"Browsing: {address}!";
+               
+            }
+            else
+            {
+                return "Invalid URL!";
+            }   
+        }
+    }
+}
+
+
