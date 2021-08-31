a29805e8300d30ed47f8b040136982acfb5f6c0e
Ralitsa <ralitsanikolova@abv.bg>
2021-08-31 11:12:45 +0300

    Commit Food Shortage

---
+++ Interfaces and Abstraction/Food Shortage/Citizen.cs
@@ -0,0 +1,15 @@
+﻿namespace Food_Shortage
+{
+    class Citizen : IBuyer
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Food { get; set; }
+        public string Birthdate { get; set; }
+        public void BuyFood()
+        {
+            Food += 10;
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Food Shortage/Rebel.cs
@@ -0,0 +1,15 @@
+﻿namespace Food_Shortage
+{
+    class Rebel : IBuyer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Group { get; set; }
+        public int Food { get; set; }
+        public void BuyFood()
+        {
+            Food += 5;
+        }
+    }
+}
+
