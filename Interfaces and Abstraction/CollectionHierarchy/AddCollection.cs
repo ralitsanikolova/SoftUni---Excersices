0cf3d32473349908b2fd53f437ff2295800f9031
Ralitsa <ralitsanikolova@abv.bg>
2021-08-31 11:15:04 +0300

    Commit Collection Hierarchy

---
+++ Interfaces and Abstraction/CollectionHierarchy/AddCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class AddCollection : ICustomCollection
+    {
+        List<string> collection = new List<string>();
+
+        public int Add(string item)
+        {
+            collection.Add(item);
+            return this.collection.Count() - 1;
+        }
+        public int Add()
+        {
+            string elementToAdd = collection.First();
+            collection.Add(elementToAdd);
+            return 0;
+        }
+    }
+}
---
+++ Interfaces and Abstraction/CollectionHierarchy/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    public class MyList : ICustomCollection
+    {
+        public int Add()
+        {
+            throw new NotImplementedException();
+        }
+        List<string> collection { get; set; } = new List<string>();
+        public string Remove()
+        {
+            string elementToRemove = collection.First();
+            collection.Remove(elementToRemove);
+            return elementToRemove;
+        }
+        public int Add(string item)
+        {
+            collection.Insert(0, item);
+            return 0;
+        }
+        public int AddLast()
+        {
+            string elementToAdd = collection.Last();
+            collection.Add(elementToAdd);
+            return 0;
+        }
+        public string RemoveFirst()
+        {
+            string elementToRemove = collection.First();
+            collection.Remove(elementToRemove);
+            return elementToRemove;
+        }
+        public int Used()
+        {
+            return collection.Count();
+        }
+    }
+}
