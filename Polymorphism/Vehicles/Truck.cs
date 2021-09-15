e869916c2d32a4dc6adbc45fd2612a2fd72ff67d
Ralitsa <ralitsanikolova@abv.bg>
2021-09-15 08:39:17 +0300

    Commit Vehicles.

---
+++ Polymorphism/Vehicles/Truck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Truck : Vehicle
+    {
+        public override void Refuel(double quantity)
+        {
+            double fuelToAdd = quantity *0.95;
+            this.FuelQuantity += fuelToAdd;
+
+            Console.WriteLine($"Truck: {this.FuelQuantity:f2}");
+        }
+        public override void DriveVehicle(double distance)
+        {
+            double neededFuel = FuelConsumption + (distance * (this.FuelConsumptionPerKm + 1.6));
+
+            if (this.FuelQuantity - neededFuel >= 0)
+            {
+                FuelQuantity -= neededFuel;
+                Console.WriteLine($"Truck travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine($"Truck needs refueling");
+            }
+        }
+    }
+}
